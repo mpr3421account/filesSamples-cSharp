eb801c66e7b93c1378c66e82f0d81ec6d41eac2b
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-01-12 17:12:34 -0300

    Files - FIleInfo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using System.IO;
 
 string sourcePath = @"C:\temp\file1.txt";
+string targetPath = @"c:\temp\file2.txt";
+
+
+try
+{
+    FileInfo file= new FileInfo(sourcePath);
+    file.CopyTo(targetPath);
+    string[] lines = File.ReadAllLines(sourcePath);//guaradará cada linha no vetor instanciado lines
+    foreach(string line in lines)
+    {
+        Console.WriteLine(line);
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("Error occurred: " + e.Message);
+}
