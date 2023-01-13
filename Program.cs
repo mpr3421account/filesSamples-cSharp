9d37d6da4daf9528f80529499d341513fe3af571
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-01-13 08:58:19 -0300

    using block.

--- Program.cs
+++ Program.cs
@@ -2,26 +2,14 @@
 
 string sourcePath = @"C:\temp\file1.txt";
 
-//FileStream fs = null;
-StreamReader sr = null;
-try
+using (FileStream fs = new FileStream(sourcePath, FileMode.Open))
 {
-    //fs = new FileStream(sourcePath, FileMode.Open);
-    
-    sr = File.OpenText(sourcePath);
-    while (!sr.EndOfStream)
+    using (StreamReader sr = new StreamReader(fs))
     {
-        string line = sr.ReadLine();
-        Console.WriteLine(line);
+        while(!sr.EndOfStream)
+        {
+            string line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
     }
-    
-}
-catch(IOException e)
-{
-    Console.WriteLine("An error occurred: " + e.Message);
-}
-finally
-{
-    if(sr != null) sr.Close();
-    //if(fs != null) fs.Close();
 }
