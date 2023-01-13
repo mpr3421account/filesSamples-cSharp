0c0d0a96aa93d3d70306664979d52c66efa54008
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-01-13 15:55:51 -0300

    Listando arquivos e criando diretorios

--- Program.cs
+++ Program.cs
@@ -5,12 +5,20 @@ string sourcePath = @"C:\temp\myfolder";
 
 try
 {
-    var folders = Directory.EnumerateDirectories(sourcePath,"*.*", SearchOption.AllDirectories);
+    var folders = Directory.EnumerateDirectories(sourcePath,"*.*", SearchOption.AllDirectories);//listando todas as pastas
     Console.WriteLine("Folders:");
     foreach(string s in folders)
     {
         Console.WriteLine(s);
     }
+    var files = Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories);//listando todos os arquivos
+    Console.WriteLine("Files:");
+    foreach (string s in files)
+    {
+        Console.WriteLine(s);
+    }
+
+    Directory.CreateDirectory(sourcePath + @"\newFolder");//criando diretorios
 }
 catch (IOException e)
 {
